890789817aa420889579b1a2daa9e8ae2868ef7c
MatasZos <B00163194@mytudublin.ie>
2026-02-10 11:30:01 +0000

    fixing connection string
    .

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,15 +1,24 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://admin:pass@roadguardiancluster.kpvvhx4.mongodb.net/?appName=roadguardiancluster";
+const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
 let clientPromise;
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your MongoDB URI to .env.local");
 }
-clientPromise = global._mongoClientPromise;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
