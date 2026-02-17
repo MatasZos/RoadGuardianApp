16d0559f911fb595f5b15f8b574f1cfbe02a4839
ETHAN2545 <B00164666@mytudublin.ie>
2026-02-17 11:21:19 +0000

    Update route.js

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,5 +1,5 @@
 import clientPromise from "../../../lib/mongodb";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
 export async function POST(req) {
   const { fullName, email, password, phone, motorbike, accountType } =
