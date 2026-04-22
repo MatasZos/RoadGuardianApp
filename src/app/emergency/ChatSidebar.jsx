285d71803059d0f52fc6cfac5ebea27937d88cb8
Mateusz-gif <b00160065@mytudublin.ie>
2026-04-22 18:17:46 +0100

    Update ChatSidebar.jsx

--- src/app/emergency/ChatSidebar.jsx
+++ src/app/emergency/ChatSidebar.jsx
@@ -1,29 +1,24 @@
 "use client";
 
-import { QUICK_REPLIES, STATUS_LABELS } from "./constants";
-import { prettify } from "./utils";
 import { styles } from "./styles";
 
-export default function ChatSidebar(props) {
-  const {
-    open,
-    onClose,
-    email,
-    conversations,
-    selectedConversation,
-    messages,
-    chatText,
-    setChatText,
-    newChatEmail,
-    setNewChatEmail,
-    chatLoading,
-    chatError,
-    selectedIncidentContext,
-    onSelectConversation,
-    onStartChat,
-    onSendMessage,
-  } = props;
-
+export default function ChatSidebar({
+  open,
+  onClose,
+  email,
+  conversations,
+  selectedConversation,
+  messages,
+  chatText,
+  setChatText,
+  newChatEmail,
+  setNewChatEmail,
+  chatLoading,
+  chatError,
+  onSelectConversation,
+  onStartChat,
+  onSendMessage,
+}) {
   return (
     <>
       {!open && (
@@ -46,6 +41,7 @@ export default function ChatSidebar(props) {
         </div>
 
         <div style={styles.chatBody}>
+          {/* LEFT */}
           <div style={styles.chatList}>
             {conversations.map((conv) => {
               const otherUser =
@@ -63,30 +59,52 @@ export default function ChatSidebar(props) {
             })}
           </div>
 
+          {/* RIGHT */}
           <div style={styles.chatPanel}>
             <div style={styles.messagesArea}>
-              {messages.map((msg, i) => (
-                <div
-                  key={i}
-                  style={
-                    msg.senderEmail === email
-                      ? styles.myMessageBubble
-                      : styles.otherMessageBubble
-                  }
-                >
-                  {msg.text}
-                </div>
-              ))}
+              {messages.map((msg, i) => {
+                const isMine = msg.senderEmail === email;
+
+                return (
+                  <div
+                    key={i}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: isMine ? "flex-end" : "flex-start",
+                      gap: "4px",
+                    }}
+                  >
+                    <div
+                      style={{
+                        ...styles.messageBubble,
+                        ...(isMine
+                          ? styles.myMessageBubble
+                          : styles.otherMessageBubble),
+                      }}
+                    >
+                      {msg.text}
+                    </div>
+
+                    <span style={styles.messageMeta}>
+                      {isMine ? "You" : msg.senderEmail}
+                    </span>
+                  </div>
+                );
+              })}
             </div>
 
             <div style={styles.messageInputRow}>
               <input
                 style={styles.chatInput}
+                placeholder="Type a message..."
                 value={chatText}
                 onChange={(e) => setChatText(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && onSendMessage()}
               />
+
               <button style={styles.startBtn} onClick={onSendMessage}>
-                Send
+                {chatLoading ? "..." : "Send"}
               </button>
             </div>
           </div>
